41f2561a0e85e7dd19413dee1426c4ece8fae15b
paulvincentarnold <paulvincentarnold@gmail.com>
2022-11-15 11:36:14 +0000

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10000000000;
+export const RES_PER_PAGE = 10;
+export const KEY = '500476c5-b773-44f5-b3c5-ea8f67ecb6e2';
+export const MODAL_CLOSE_SEC = 2.5;
