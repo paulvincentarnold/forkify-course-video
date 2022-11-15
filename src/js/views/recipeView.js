bb0560bd61b2434b389b112d2620a66f0bd75326
paulvincentarnold <paulvincentarnold@gmail.com>
2022-11-15 13:56:44 +0000

    Fix fraction issue

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from './View.js';
 // import icons from '../img/icons.svg'; // Parcel 1
 
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import {Fraction} from 'fractional';
+import fracty from 'fracty';
 
 class RecipeView extends View {
     _parentElement = document.querySelector('.recipe');
@@ -113,7 +113,7 @@ class RecipeView extends View {
             <svg class="recipe__icon">
             <use href="${icons}#icon-check"></use>
             </svg>
-            <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+            <div class="recipe__quantity">${ing.quantity ? fracty(ing.quantity).toString() : ''}</div>
             <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
             ${ing.description}
